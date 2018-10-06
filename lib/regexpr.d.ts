549489353236cc48b4b34089303632e05f4ef6e4
luigisenzamici <luigisenzamici@outlook.it>
2018-10-06 22:06:58 +0200

    finish

---
+++ lib/regexpr.d.ts
@@ -0,0 +1,13 @@
+export declare const email: RegExp;
+export declare const cap: RegExp;
+export declare const number: RegExp;
+export declare const noNumber: RegExp;
+export declare const integer: RegExp;
+export declare const float: RegExp;
+export declare const negative: RegExp;
+export declare const genericPhoneNumber: RegExp;
+export declare const codiceFiscale: RegExp;
+/**
+ * italianMobilePhone [warning not update]
+ */
+export declare const italianMobilePhone: RegExp;
---
+++ src/regexpr.ts
@@ -0,0 +1,14 @@
+
+export const email= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const cap= /^(V-|I-)?[0-9]{5}$/;
+export const number= /^-?\d+$/;
+export const noNumber= /^([^0-9]*)$/;
+export const integer= /^\d+$/;
+export const float= /[+-]?([0-9]*[.])?[0-9]+/;
+export const negative= /^-\d*\.?\d+$/;
+export const genericPhoneNumber = /(^[0-9\/\\]*)$/;
+export const codiceFiscale =/^(?:(?:[B-DF-HJ-NP-TV-Z]|[AEIOU])[AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[1256LMRS][\dLMNP-V])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i;
+/**
+ * italianMobilePhone [warning not update]
+ */
+export const italianMobilePhone= /^([+]39)?((38[{8,9}|0])|(34[{7-9}|0])|(36[6|8|0])|(33[{3-9}|0])|(32[{8,9}]))([\d]{7})$/;
