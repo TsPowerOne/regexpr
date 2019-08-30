142787c250108df19b86e8a4b54a76258b0c24ce
Luigi Senzamici <luigi.senzamici.external@yoox.com>
2019-08-30 13:56:39 +0200

    test added and azure pipeline added

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,1 +1,23 @@
-//empty for now
+import { float } from'../lib/regexpr';
+import { expect } from 'chai';
+
+
+let floatValue = [ "-0.2", "0.1", "123.1123"];
+let notFloatValue = [ "1.text", "333/333"];
+
+describe('float regexpr', () => {
+
+    floatValue.forEach(e=>{
+        it(`if ${e} should return true`, () => {
+            
+          expect(float.test(e)).to.equal(true);
+        });
+    });
+ 
+    notFloatValue.forEach(e=>{
+        it(`if ${e} should return false`, () => {
+    
+          expect(float.test(e)).to.equal(false);
+        });
+    });
+});
